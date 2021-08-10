c8b485035127899566896da12c7f1082b6960754
Sanketh7 <sankethv03@gmail.com>
2021-08-10 05:31:31 +0000

    Deploying to gh-pages from @ Sanketh7/sanketh7.github.io@2a10c4b49da21e3feb3a561b9f23698b5f98f6b3 🚀

--- _next/static/Y_G68Bb4tZNu1x9pY-6x6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2177e21b49bd3c61a79d.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
