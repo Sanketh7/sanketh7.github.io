2a10c4b49da21e3feb3a561b9f23698b5f98f6b3
Sanketh Varamballi <sankethv03@gmail.com>
2021-08-09 22:30:03 -0700

    fixed icon hover css issue

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -1,4 +1,3 @@
-import {ClickableIcon} from './ClickableIcon';
 import {GithubIcon} from './icons/GithubIcon';
 import {SkillPill} from './SkillPill';
 import {IProject} from '../lib/projects';
@@ -10,9 +9,7 @@ export const ProjectItem: React.FC<Props> = ({title, description, skills, github
     <>
       <div className="col-span-1 flex flex-col items-center md:items-end">
         <div className="text-3xl font-mono flex-grow mb-4 truncate">{title}</div>
-        <ClickableIcon defaultColor="text-gray-100" hoverColor="text-green-300" href={github}>
-          <GithubIcon styles="w-12 fill-current" />
-        </ClickableIcon>
+        <GithubIcon styles="w-12 fill-current text-gray-100 hover:text-green-300" href={github} />
       </div>
       <div className="col-span-2 flex flex-col items-center md:items-start">
         <div className="font-mono flex-grow mb-4 text-center md:text-left">{description}</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import {ClickableIcon} from '../components/ClickableIcon';
 import {EducationItem} from '../components/EducationItem';
 import {EducationList} from '../components/EducationList';
 import {EmailIcon} from '../components/icons/EmailIcon';
@@ -29,15 +28,9 @@ const Home: React.FC<Props> = ({projects, education}: Props) => {
       <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-green-400 to-blue-500">
         <div className="text-2xl sm:text-4xl md:text-6xl font-bold text-gray-100 font-mono mb-8">Sanketh Varamballi</div>
         <div className="flex items-center justify-around w-1/2">
-          <ClickableIcon defaultColor="text-gray-100" hoverColor="text-green-300" href="https://www.github.com/sanketh7">
-            <GithubIcon styles="w-8 sm:w-12 md:w-16 fill-current" />
-          </ClickableIcon>
-          <ClickableIcon defaultColor="text-gray-100" hoverColor="text-green-300" href="https://www.linkedin.com/in/sankethvaramballi">
-            <LinkedinIcon styles="w-8 sm:w-12 md:w-16 fill-current" />
-          </ClickableIcon>
-          <ClickableIcon defaultColor="text-gray-100" hoverColor="text-green-300" href="mailto:sankethv@umd.edu">
-            <EmailIcon styles="w-8 sm:w-12 md:w-16 fill-current" />
-          </ClickableIcon>
+          <GithubIcon styles="w-8 sm:w-12 md:w-16 fill-current text-gray-100 hover:text-green-300" href="https://www.github.com/sanketh7"/>
+          <LinkedinIcon styles="w-8 sm:w-12 md:w-16 fill-current text-gray-100 hover:text-green-300" href="https://www.linkedin.com/in/sankethvaramballi"/>
+          <EmailIcon styles="w-8 sm:w-12 md:w-16 fill-current text-gray-100 hover:text-green-300" href="mailto:sankethv@umd.edu"/>
         </div>
       </div>
 
